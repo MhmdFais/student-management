88b9fa986101a774c56abd99c706919d0abee071
404CoderM <117585798+404CoderM@users.noreply.github.com>
2024-03-27 06:49:29 +0530

    Add Student and Update Student forms

---
+++ Back-End/src/main/java/com/sms/BackEnd/model/Student.java
@@ -0,0 +1,13 @@
+package com.sms.BackEnd.model;
+
+import jakarta.persistence.Entity;
+
+//@Entity
+//public class Student {
+//    private int regNo;
+//    private String firstName;
+//    private String lastName;
+//    private int phoneNo;
+//    private String address;
+//
+//}
